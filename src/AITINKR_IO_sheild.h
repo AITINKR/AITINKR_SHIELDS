de7b61de655bac76e69e6d8b9fe21b315da717f3
KARTHIKEYA210305 <karthikeyagattu1@gmail.com>
2023-10-16 18:31:59 +0530

    added the source files for the library publish
    added the source files for the library publish

---
+++ src/AITINKR_IO_sheild.h
@@ -0,0 +1,37 @@
+#ifndef AITINKR_IO_sheild_h
+#define AITINKR_IO_sheild_h
+
+
+class Ultrasonic {
+
+public:
+
+  Ultrasonic(uint8_t port);
+  unsigned int read();
+
+private:
+  uint8_t trig;
+  uint8_t echo;
+  unsigned long previousMicros;
+  unsigned long timeout;
+  unsigned int getDis();
+};
+
+class motor {
+
+public:
+  void motor::init();
+  void motor::setSpeed(uint8_t sp);
+  void motor::forword();
+  void motor::backword();
+  void motor::right();
+  void motor::left();
+  void motor::stop();
+
+
+private:
+};
+
+
+
+#endif  // AITINKR_IO_sheild_v1_h
